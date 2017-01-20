08d24e3c4fc59a32541776a380ec07fea8046971
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-19 22:50:32 -0500

    testing a couple of moves using cypress

--- cypress/integration/2048-kensho-spec.js
+++ cypress/integration/2048-kensho-spec.js
@@ -1,9 +1,19 @@
-describe('2048-kensho', function(){
-  beforeEach(function(){
+describe('2048-kensho', function () {
+  beforeEach(function () {
     cy.visit('/')
   })
-  it('has 2048 title', function(){
-    // https://on.cypress.io/api/title
+  it('has 2048 title', function () {
     cy.title().should('include', '2048')
+  })
+  it('starts with 3 tiles', () => {
+    cy.get('.tile-container')
+      .find('.tile')
+      .should('have.length', 3)
+  })
+  it('can play a couple of moves', () => {
+    cy.left()
+    cy.right()
+    cy.up()
+    cy.down()
   })
 })
