4caadb14280c0da937a26adeaa5f3d14f9dc5aed
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-19 21:53:17 -0500

    testing any website by changing baseUrl

--- cypress/integration/2048-kensho-spec.js
+++ cypress/integration/2048-kensho-spec.js
@@ -1,6 +1,6 @@
 describe('2048-kensho', function(){
   beforeEach(function(){
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
   })
   it('has 2048 title', function(){
     // https://on.cypress.io/api/title
